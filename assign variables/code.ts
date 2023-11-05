f28b8b4bfc1379254e744fe81bf0498d22414d84
Haentjens <haentjensquentin@gmail.com>
2023-11-05 12:46:00 +0100

    Merge branch 'assign-color'

--- assign variables/code.ts
+++ assign variables/code.ts
@@ -1,5 +1,5 @@
 figma.parameters.on('input', ({query, result, key, parameters}) =>{
-
+if(key === 'color'){
   let colorNames: string[] = [];
   let dataNames: string[] = [];
 
@@ -28,7 +28,7 @@ figma.parameters.on('input', ({query, result, key, parameters}) =>{
           if(getValueMode){
             let firstModeValue = getValueMode[Object.keys(getValueMode)[0]] as RGBA;
 
-            colorNames.push(getColor);
+            colorNames.push(getColor + ' (' + collectionName + ')');
             dataNames.push(rgbaToHex(firstModeValue))
           }
         }
@@ -40,13 +40,48 @@ figma.parameters.on('input', ({query, result, key, parameters}) =>{
     // result.setSuggestions([
     //   { name: 'NOM', icon: `data:image/svg+xml;base64,${iconBase64}` },
     // ]);
-
+  }
+  
 })
 
 figma.on('run', ({ command, parameters }: RunEvent) => {
+
+  if(command == 'color' && parameters){
+    var variableColors: { [key: string]: any } = figma.variables.getLocalVariables("COLOR"),
+        variableColorsLength = variableColors.length;
+
+    for(let colorNumber = 0; colorNumber < variableColorsLength; colorNumber++) {
+      let variableColorsId = variableColors[colorNumber].id;
+
+      if(parameters[command].toLowerCase().includes(variableColors[colorNumber].name.toLowerCase())) {
+
+        let ChoiceColorId = variableColors[colorNumber];
+        const figmaSelection = figma.currentPage.selection;
+        
+
+        for(let mySelectionCount = 0; mySelectionCount < figmaSelection.length; mySelectionCount++){
+
+          const mySelection: RectangleNode = figma.currentPage.selection[mySelectionCount] as RectangleNode;
+        
+          if(ChoiceColorId){
+            const fillsCopy = clone(mySelection.fills);
+            console.log(fillsCopy[0]);
+
+            fillsCopy[0] = figma.variables.setBoundVariableForPaint(fillsCopy[0], 'color', ChoiceColorId)
+            mySelection.fills = fillsCopy
+        
+            // If you want reselect after selection
+          //  figma.currentPage.selection = [mySelection];
+          }
+        }
+        
+        
+      } 
+    }
+  }
   // Votre code à exécuter lorsque l'événement "run" est déclenché
-  console.log(`Commande: ${command}`);
-  console.log(`Paramètres: ${parameters}`);
+  // console.log(`Commande: ${command}`);
+  // console.log(`Paramètres: ${parameters}`);
 
   figma.closePlugin()
 });
@@ -57,4 +92,8 @@ function rgbaToHex(color: RGBA): string {
   const g = (Math.round(color.g * 255).toString(16).length === 1 ? '0' : '') + Math.round(color.g * 255).toString(16);
   const b = (Math.round(color.b * 255).toString(16).length === 1 ? '0' : '') + Math.round(color.b * 255).toString(16);
   return `#${r}${g}${b}`;
+}
+
+function clone(val: any) {
+  return JSON.parse(JSON.stringify(val))
 }
