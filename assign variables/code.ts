48910f909cdae7839d9c5390c6ebb1f638b744e1
Haentjens <haentjensquentin@gmail.com>
2023-11-13 06:18:43 +0100

    create function and width and height parameters

--- assign variables/code.ts
+++ assign variables/code.ts
@@ -1,91 +1,176 @@
 figma.parameters.on('input', ({query, result, key, parameters}) =>{  
-if(key === 'color' || key === "stroke_color"){
-  let colorNames: string[] = [];
-  let dataNames: string[] = [];
-  
-  let getCollection = figma.variables.getLocalVariableCollections();
-  let collectionLength = getCollection.length;
 
-    //Collection Loop
-    for (let collectionCount = 0; collectionCount < collectionLength; collectionCount++) {
-      let collection = getCollection[collectionCount];
-      let collectionName = collection.name;
-      let collectionVariablesIds = collection.variableIds;
-      let collectionVariablesIdsLength = collectionVariablesIds.length;
-
-      //Variables Loop
-      for (let variableIdsCount = 0; variableIdsCount < collectionVariablesIdsLength; variableIdsCount++) {
-        let variableIdsString = collectionVariablesIds[variableIdsCount];
-        let variableObjet = figma.variables.getVariableById(`${variableIdsString}`);
-        let getColor = variableObjet?.name.toLocaleLowerCase();
-        let getValueMode = variableObjet?.valuesByMode;
-        let getResolveType = variableObjet?.resolvedType;
-
-        if(getResolveType == 'COLOR' && getColor !== undefined) {
-          //Push the color name to the array
-          if(getValueMode){
-            let firstModeValue = getValueMode[Object.keys(getValueMode)[0]] as RGBA;
-
-            colorNames.push(getColor + ' (' + collectionName + ')');
-            dataNames.push(rgbaToHex(firstModeValue));
-          }
-        }
-      }
-    }
-    const suggestions = colorNames
-        .filter(s => s.includes(query))
-        .map((s, index) => {
-          const myHex = dataNames[index];
-
-          return ({ name: s, icon: `<svg width="$size$" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="40" height="40" fill="${myHex}" /></svg>`})
-        });
-         
-    result.setSuggestions(suggestions);
-
-
-  } else if (key === "stroke_width") {
-    let defaultStrokeSize: string[] = [];
+  switch(key){
+    case 'color':
+    case 'border_color':
+      let colorNames: string[] = [];
+    let dataNames: string[] = [];
+    
     let getCollection = figma.variables.getLocalVariableCollections();
     let collectionLength = getCollection.length;
-    let floatVariableCount = figma.variables.getLocalVariables('FLOAT').length;
 
-    //Collection Loop
-    for (let collectionCount = 0; collectionCount < collectionLength; collectionCount++) {
-      let collection = getCollection[collectionCount];
-      let collectionName = collection.name;
-      let collectionVariablesIds = collection.variableIds;
-      let collectionVariablesIdsLength = collectionVariablesIds.length;
+      //Collection Loop
+      for (let collectionCount = 0; collectionCount < collectionLength; collectionCount++) {
+        let collection = getCollection[collectionCount];
+        let collectionName = collection.name;
+        let collectionVariablesIds = collection.variableIds;
+        let collectionVariablesIdsLength = collectionVariablesIds.length;
 
-      //Variables Loop
-      for (let variableIdsCount = 0; variableIdsCount < collectionVariablesIdsLength; variableIdsCount++) {
-        let variableIdsString = collectionVariablesIds[variableIdsCount];
-        let variableObjet = figma.variables.getVariableById(`${variableIdsString}`);
-        let getColor = variableObjet?.name;
-        let getValueMode = variableObjet?.valuesByMode;
-        let getResolveType = variableObjet?.resolvedType;
+        //Variables Loop
+        for (let variableIdsCount = 0; variableIdsCount < collectionVariablesIdsLength; variableIdsCount++) {
+          let variableIdsString = collectionVariablesIds[variableIdsCount];
+          let variableObjet = figma.variables.getVariableById(`${variableIdsString}`);
+          let getColor = variableObjet?.name.toLocaleLowerCase();
+          let getValueMode = variableObjet?.valuesByMode;
+          let getResolveType = variableObjet?.resolvedType;
 
-        if(getResolveType == 'FLOAT' && floatVariableCount > 0) {
-          if(getValueMode){
-            let firstModeValue = getValueMode[Object.keys(getValueMode)[0]] as Number;
-            defaultStrokeSize.push(firstModeValue + 'px  (' + collectionName + ')');
+          if(getResolveType == 'COLOR' && getColor !== undefined) {
+            //Push the color name to the array
+            if(getValueMode){
+              let firstModeValue = getValueMode[Object.keys(getValueMode)[0]] as RGBA;
+
+              colorNames.push(getColor + ' (' + collectionName + ')');
+              dataNames.push(rgbaToHex(firstModeValue));
+            }
           }
         }
       }
-    }
-    if(floatVariableCount === 0) {
-      defaultStrokeSize.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
-    }
+      const suggestionsColor = colorNames
+          .filter(s => s.includes(query))
+          .map((s, index) => {
+            const myHex = dataNames[index];
 
-    const suggestions = defaultStrokeSize
-        .filter(s => s.includes(query))
-        .map((s, index) => {
-          const variableStringToNumber = extractNumber(defaultStrokeSize[index]) as number; 
-          const sizeFixe = 16;          
+            return ({ name: s, icon: `<svg width="$size$" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="40" height="40" fill="${myHex}" /></svg>`})
+          });
+          
+      result.setSuggestions(suggestionsColor);
+    break;
 
-          return ({ name: s, icon: `<svg width="${sizeFixe}" height="${sizeFixe}" viewBox="0 0 ${sizeFixe} ${sizeFixe}" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="${sizeFixe}" height="${sizeFixe}" fill="#ffffff" stroke="red" stroke-width="${variableStringToNumber}"/></svg>`});
+    case 'border_width':
+
+      let defaultStrokeSize: string[] = [];
+      let getCollectionStroke = figma.variables.getLocalVariableCollections();
+      let collectionLengthStroke = getCollectionStroke.length;
+      let floatVariableCount = figma.variables.getLocalVariables('FLOAT').length;
+
+      //Collection Loop
+      for (let collectionCount = 0; collectionCount < collectionLengthStroke; collectionCount++) {
+        let collection = getCollectionStroke[collectionCount];
+        let collectionName = collection.name;
+        let collectionVariablesIds = collection.variableIds;
+        let collectionVariablesIdsLength = collectionVariablesIds.length;
+
+        //Variables Loop
+        for (let variableIdsCount = 0; variableIdsCount < collectionVariablesIdsLength; variableIdsCount++) {
+          let variableIdsString = collectionVariablesIds[variableIdsCount];
+          let variableObjet = figma.variables.getVariableById(`${variableIdsString}`);
+          let getColor = variableObjet?.name;
+          let getValueMode = variableObjet?.valuesByMode;
+          let getResolveType = variableObjet?.resolvedType;
+
+          if(getResolveType == 'FLOAT' && floatVariableCount > 0) {
+            if(getValueMode){
+              let firstModeValue = getValueMode[Object.keys(getValueMode)[0]] as Number;
+              defaultStrokeSize.push(firstModeValue + 'px  (' + collectionName + ')');
+            }
+          }
+        }
+      }
+      if(floatVariableCount === 0) {
+        defaultStrokeSize.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');
+      }
+
+      const suggestionsNumber = defaultStrokeSize
+          .filter(s => s.includes(query))
+          .map((s, index) => {
+            const variableStringToNumber = extractNumber(defaultStrokeSize[index]) as number; 
+            const sizeFixe = 16;          
+
+            return ({ name: s, icon: `<svg width="${sizeFixe}" height="${sizeFixe}" viewBox="0 0 ${sizeFixe} ${sizeFixe}" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="${sizeFixe}" height="${sizeFixe}" fill="#ffffff" stroke="red" stroke-width="${variableStringToNumber}"/></svg>`});
+          });
+          
+      result.setSuggestions(suggestionsNumber);
+    break;
+
+    case 'height':
+    case 'width':
+      
+      // let getCollectionNumber = figma.variables.getLocalVariableCollections();
+      // let collectionLengthNumber = getCollectionNumber.length;
+      
+      // let numberVariableCount = figma.variables.getLocalVariables('FLOAT').length;
+      // let collectionCount = 0;
+
+      // //Collection Loop
+      // // for (let collectionCount = 0; collectionCount < collectionLengthNumber; collectionCount++) {
+      // getCollectionNumber.forEach(element => {
+      //   let collection = getCollectionNumber[collectionCount];
+      //   let variableIdsCount = 0;
+      //   let scopeWidthHeight= false;
+      //   // console.log(element.name);
+        
+
+      //   //Variables Loop
+      //   numberVariable.forEach(element => {
+      //     let variableIdsString = collection.variableIds[variableIdsCount];
+      //     let variableObjet = figma.variables.getVariableById(`${variableIdsString}`);
+      //     let scopeCounter = 0;
+          // console.log(element.scopes)
+          // console.log(element)
+          // if(element.scopes.includes(chaineRecherchee))
+
+          // element.scopes.forEach(element => {
+          //   console.log(element);
+            
+            
+          // });
+
+          // variableObjet?.scopes.forEach(element => {
+            
+          //   if(element == 'WIDTH_HEIGHT'){
+          //     if(variableObjet?.resolvedType == 'FLOAT' && variableObjet?.valuesByMode) {                
+          //         let firstModeValue = variableObjet?.valuesByMode[Object.keys(variableObjet?.valuesByMode)[0]] as Number;
+          //         heightArray.push(firstModeValue + 'px  (' + collection?.name + ')');
+          //     }
+          //     scopeCounter++;
+          //     scopeWidthHeight = true;
+          //   } 
+          // });
+          // console.log(heightArray)
+
+          // if(scopeWidthHeight) {
+          //   // console.log('la longueur du tableau est 0');
+            
+          //   if(variableObjet?.resolvedType == 'FLOAT' && variableObjet?.valuesByMode) {
+          //       let firstModeValue = variableObjet?.valuesByMode[Object.keys(variableObjet?.valuesByMode)[0]] as Number;
+          //       heightArray.push(firstModeValue + 'px  (' + collection?.name + ')');
+          //   }
+          // }
+      //     variableIdsCount++;
+      //   });
+
+      //   collectionCount++;
+      // });
+      // }
+      let numberVariable = figma.variables.getLocalVariables('FLOAT'),
+          heightArray: string[] = [],
+          conditionalWidthHeight = numberVariable.some(numberVariable => numberVariable.scopes.toString().includes('WIDTH_HEIGHT'));
+
+      if(conditionalWidthHeight) {
+        numberVariable.forEach(element => {
+          if(element){
+            writeVariables(element, "WIDTH_HEIGHT", heightArray);
+          }
         });
-         
-    result.setSuggestions(suggestions);
+      } else {
+        numberVariable.forEach(element => {
+          if(element){
+            writeVariables(element, "WIDTH_HEIGHT", heightArray, false);
+          }
+        });
+      }
+      result.setSuggestions(heightArray.filter(s => s.includes(query)))
+    break;
   }
 })
 
@@ -120,10 +205,9 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
         }
       break;
 
-      case 'stroke':
+      case 'border':
         var variableColors: { [key: string]: any } = figma.variables.getLocalVariables("COLOR"),
             variableColorsLength = variableColors.length;
-        const figmaSelection = figma.currentPage.selection;
 
         for(let colorNumber = 0; colorNumber < variableColorsLength; colorNumber++) {
           if(parameters[Object.keys(parameters)[0]].toLowerCase().includes(variableColors[colorNumber].name.toLowerCase())) {
@@ -133,6 +217,8 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
             
             for(let mySelectionCount = 0; mySelectionCount < figmaSelection.length; mySelectionCount++){              
               const mySelection: RectangleNode = figmaSelection[mySelectionCount] as RectangleNode;
+              
+              
               if(ChoiceColorId){
                 if(mySelection.strokes.length === 0){
                   mySelection.strokes = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }];
@@ -140,16 +226,31 @@ figma.on('run', ({ command, parameters }: RunEvent) => {
                 const strokesCopy = clone(mySelection.strokes);
     
                 strokesCopy[0] = figma.variables.setBoundVariableForPaint(strokesCopy[0], 'color', ChoiceColorId)
-                mySelection.strokes = strokesCopy;
-                if(parameters.stroke_width != undefined){
-                  const numberStrokeWidth = extractNumber(parameters.stroke_width) as number;
+                mySelection.strokes = strokesCopy;                
+
+                if(parameters.border_width != undefined){
+                  const numberStrokeWidth = extractNumber(parameters.border_width) as number;
                   mySelection.strokeWeight = numberStrokeWidth;
+                } else {
+                  mySelection.strokeWeight = 1;
                 }
               
               }
             }
           }
         } 
+      break;
+
+      case 'height':
+      case 'width':
+        
+        const maSelection: RectangleNode = figma.currentPage.selection[0] as RectangleNode;
+        const maVariable = figma.variables.getVariableById('VariableID:177:26');
+        if(maVariable){
+          maSelection.setBoundVariable(command, maVariable.id);
+        }
+        
+
       break;
     }
   }
@@ -171,4 +272,18 @@ function clone(val: any) {
 function extractNumber(str: any) {
   const matches = str.match(/\d+/);
   return matches ? parseInt(matches[0], 10) : null;
+}
+
+function writeVariables(contextualElement: any, numberScoping: string, myArray: any, checkCondition: boolean = true){
+  const getNumberCollection = contextualElement.variableCollectionId,
+        getNumberCollectionInfo = figma.variables.getVariableCollectionById(getNumberCollection),
+        variableValueNumber = contextualElement.valuesByMode[Object.keys(contextualElement.valuesByMode)[0]] as Number;
+
+  if(checkCondition && contextualElement.scopes.toString().includes(numberScoping)){
+    myArray.push(variableValueNumber + 'px (' + getNumberCollectionInfo?.name + ')');
+  }
+
+  if(!checkCondition){
+    myArray.push(variableValueNumber + 'px (' + getNumberCollectionInfo?.name + ')');
+  }
 }
